ce7dea18ccbd77da8181c02d7312080e79f19ad2
Alexandr <f.alexandr08@mail.ru>
2025-03-25 23:13:47 +0500

    Add files via upload

---
+++ docs/server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,17 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+    <meta charset="utf-8">
+    <title>IT-Company</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="/image/project.png">
+<style>*{margin:0;padding:0;box-sizing:border-box}body{width:100%;height:100%;overflow-x:hidden;font-family:sans-serif;background:#fff}</style><link rel="stylesheet" href="styles-K2FWRRNJ.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-K2FWRRNJ.css"></noscript></head>
+<body ngcm="">
+    <app-root></app-root>
+
+    <!-- <script src="./polyfills.ts"></script>
+    <script src="./main.ts"></script> -->
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-XBSXSYVZ.js" type="module"></script></body>
+</html>
+`;
